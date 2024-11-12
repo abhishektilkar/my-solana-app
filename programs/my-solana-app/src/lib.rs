ac3edd5e3f5ce8a96746f249772c0c9522dc970d
abhishektilkar <tilkarabhishek2000@gmail.com>
2024-11-12 23:21:45 +0530

    Game code completed val

--- programs/my-solana-app/src/lib.rs
+++ programs/my-solana-app/src/lib.rs
@@ -8,9 +8,70 @@ pub mod my_solana_app {
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        ctx.accounts.new_game_data_account.set_inner(GameDataAccount {
+            player_position: 0
+        });
+
+        Ok(())
+    }
+
+    pub fn move_right(ctx: Context<MoveRight>) -> Result<()> {
+        // const 
+        let game_account = &mut ctx.accounts.new_game_data_account;
+        game_account.player_position += 1;
+        Ok(())
+    }
+
+    pub fn move_left(ctx: Context<MoveLeft>) -> Result<()> {
+        let game_account = &mut ctx.accounts.new_game_data_account;
+        game_account.player_position -= 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        // init_if_needed,
+        init,
+        seeds = [b"level1"],
+        bump,
+        payer = signer,
+        space = 8+1
+    )]
+    pub new_game_data_account: Account<'info, GameDataAccount>,
+
+    #[account(
+        mut
+    )]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct MoveRight<'info> {
+    #[account(
+        mut,
+        seeds = [b"level1"],
+        bump
+    )]
+    pub new_game_data_account: Account<'info, GameDataAccount>
+}
+
+#[derive(Accounts)]
+pub struct MoveLeft<'info> {
+    #[account(
+        mut,
+        seeds = [b"level1"],
+        bump
+    )]
+    pub new_game_data_account: Account<'info, GameDataAccount>
+}
+
+#[account]
+pub struct GameDataAccount {
+    pub player_position: u8
+}
+
+
